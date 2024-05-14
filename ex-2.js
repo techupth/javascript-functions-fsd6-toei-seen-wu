9d5f524f76aa7a5df9af388720d2e4b14349a132
Apirak Fakin <apirakfakin@gmail.com>
2024-05-14 13:13:55 +0700

    feat: add ex-2

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,12 @@
-// Start coding here
+const add = function (a, b) {
+   return a + b;
+ }
+const subtract = function (a, b) {
+   return a + b;
+ }
+ const multiply = function (a, b) {
+   return a * b;
+ }
+ const divide = function (a, b) {
+   return a / b;
+ }
